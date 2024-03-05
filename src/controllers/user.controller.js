7e198a964d959408a88c92ad9ee07709374643d8
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-03-05 21:17:35 +0530

    Add getChannelprofile controller function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -350,3 +350,80 @@ export const updateCoverImage = asyncHandler(async (req, res) => {
     })
   );
 });
+
+export const getChannelprofile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        email: 1,
+        avatar: 1,
+        username: 1,
+        fullname: 1,
+        coverImage: 1,
+        isSubscribed: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+      },
+    },
+  ]);
+  
+  console.log(channel);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel not found");
+  }
+
+  return res.status(200).json(
+    new ApiResponse({
+      statusCode: 200,
+      data: { channel: channel[0] },
+      message: "Channel retrieved successfully",
+    })
+  );
+});
